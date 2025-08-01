9cf477c5c6fc6dcbf044d4265ef46f39de5667e1
apoorvathakur25007-commit <apoorvathakur25007@gmail.com>
2025-08-01 17:20:08 +0530

    Add files via upload

---
+++ A-1 Task-1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+add=a+b
+sub=a-b
+multi=a*b
+div=a/b
+print("\nAddition: ",add,"\nSubtraction: ",sub,"\nMultiplication: ",multi,"\nDivision: ",div)
---
+++ A-1 Task-2.py
@@ -0,0 +1,4 @@
+a=input("Enter your first name: ")
+b=input("Enter your last name: ")
+full=a+b
+print("\nHello,",full+"!","Welcome to the Python program.")
